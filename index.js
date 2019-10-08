6361977760b97cbb4e68c73f74dc734fdc1fc599
Bradley O'Neill <bradley.oneill@gmail.com>
2019-10-08 14:42:26 +0100

    Added new const (3 - 4) and end point routes (63 - 99)

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express'),
-	morgan = require('morgan');
+	  morgan = require('morgan'),
+	  bodyParser = require("body-parser"),
+	  uuid = require("uuid");
 
 const app = express();
 
@@ -49,6 +51,8 @@ app.use(express.static('public'));
 
 app.use(morgan('common'));
 
+app.use(bodyParser.json());
+
 app.use(function (err, req, res, next) {
 	console.error(err.stack);
 	res.status(500).send('Oops! Sorry about that, something went wrong!');
@@ -56,18 +60,42 @@ app.use(function (err, req, res, next) {
 
 // GET requests
 app.get('/movies', function (req, res) {
-	res.json(favMovies)
+	res.send("Successful GET request returning data about all movies.");
 });
 
-app.get('/', function (req, res) {
-	res.send('Welcome to MyFlix')
+app.get('/movies/title', function (req, res) {
+	res.send("Successful GET request returning data about a single movie by title.");
 });
 
-app.get('/documentation', function (req, res) {
-	res.sendFile('documentation.html', {
-		root: __dirname
-	})
+app.get('/movies/genre/genre', function (req, res) {
+	res.send("Successful GET request returning data about a single movie genre by genre type.");
 });
+
+app.get('/movies/dir/name', function (req, res) {
+	res.send("Successful GET request returning data about a director by name.");
+});
+
+app.post('/register/user', function (req, res) {
+	res.send("Successfully registered.");
+});
+
+app.put('/update/user/id', function (req, res) {
+	res.send("Successfully updated information.");
+});
+
+app.post('/favourites/id/title', function (req, res) {
+	res.send("Movie successfully added to favourite.");
+});
+
+app.delete('/favourites/id/title', function (req, res) {
+	res.send("Movie successfully removed from favourites.");
+});
+
+app.delete('/register/user/id', function (req, res) {
+	res.send("User successfully deregistered");
+});
+
+
 
 // listening for requests
 app.listen(8080, () =>
