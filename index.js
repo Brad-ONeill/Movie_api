83fe4daa2a7734967a9aeabff1b4b998b0d54371
Brad <49478841+Brad-ONeill@users.noreply.github.com>
2020-03-31 10:37:29 +0100

    updated server login info

--- index.js
+++ index.js
@@ -24,9 +24,17 @@ const {
     validationResult
 } = require('express-validator');
 
-mongoose.connect('mongodb://localhost:27017/myflixdb', {
-    useNewUrlParser: true
-});
+
+
+mongoose.connect('process.env.CONNECTION_URI', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+/*mongoose.connect('mongodb://localhost:27017/myflixdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});*/
 
 //Middleware
 app.use(express.static('public'));
