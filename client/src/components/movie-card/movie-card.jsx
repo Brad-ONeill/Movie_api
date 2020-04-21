7d621f218d1443fa4ffb700454babce23e04f7c6
Brad <49478841+Brad-ONeill@users.noreply.github.com>
2020-04-21 16:34:56 +0100

    Updated movie-card PropTypes
    Removed .isrequired from movie elements (Title, Description & ImagePath) and added additional types: Director (Name) and Genre (Name & Description)

--- client/src/components/movie-card/movie-card.jsx
+++ client/src/components/movie-card/movie-card.jsx
@@ -22,9 +22,16 @@ export class MovieCard extends React.Component {
 
 MovieCard.propTypes = {
     movie: PropTypes.shape({
-        Title: PropTypes.string.isRequired,
-        Description: PropTypes.string.isRequired,
-        ImagePath: PropTypes.string.isRequired,
+        Title: PropTypes.string,
+        Description: PropTypes.string,
+        Director: PropTypes.exact({
+            Name: PropTypes.string
+        }),
+        ImagePath: PropTypes.string,
+        Genre: PropTypes.exact({
+            Name: PropTypes.string,
+            Description: PropTypes.string
+        })
     }).isRequired,
     onClick: PropTypes.func.isRequired
 };
