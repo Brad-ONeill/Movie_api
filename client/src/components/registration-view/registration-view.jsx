59a6a9d70a3f53f620ba186465f615f3c0b81b2c
Brad <49478841+Brad-ONeill@users.noreply.github.com>
2020-04-22 14:33:41 +0100

    Update registration-view.jsx

--- client/src/components/registration-view/registration-view.jsx
+++ client/src/components/registration-view/registration-view.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 //importing scss file
-import './registration view.scss';
+import './registration-view.scss';
 
 export function RegistrationView(props) {
     const [name, setName] = useState('');
