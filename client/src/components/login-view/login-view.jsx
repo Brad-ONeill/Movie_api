0030ecdcaf36af30d3a8a456ac8a07f833af3272
Brad <49478841+Brad-ONeill@users.noreply.github.com>
2020-04-22 12:58:13 +0100

    Created basic form (no styling) for LoginView
    Created basic login form (always logged in) and added {useState} to import to preserve state between functions

--- client/src/components/login-view/login-view.jsx
+++ client/src/components/login-view/login-view.jsx
@@ -1,5 +1,28 @@
-const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(username, password); // Send a request to the server for authentication then call props.onLoggedIn(username)
-    props.onLoggedIn(username);
-};
+import React, { useState } from 'react'; //{useState} to preserve state between renders
+
+//Import stylings
+import './login-view.scss';
+
+export function LoginView(props) {
+    const [username, setUsername] = useState(''); //Set prop state to blank
+    const [password, setPassword] = useState(''); //Set prop state to blank
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(username, password); // Send a request to the server for authentication then call props.onLoggedIn(username)
+        props.onLoggedIn(username);
+    };
+
+    //Return basic form
+    return (
+        <form>
+            <label> Username:
+                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+            </label>
+            <label> Password:
+                <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+            </label>
+            <button type="button" onClick={handleSubmit}>Login</button>
+        </form>
+    );
+}
