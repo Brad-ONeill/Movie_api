0d6cda6d9708a79cde568ac60548b53b095b745e
Brad <49478841+Brad-ONeill@users.noreply.github.com>
2020-04-22 14:03:36 +0100

    Creation of basic form field and corresponding export function (prop)

--- client/src/components/registration-view/registration-view.jsx
+++ client/src/components/registration-view/registration-view.jsx
@@ -3,3 +3,27 @@ import PropTypes from 'prop-types';
 
 //importing scss file
 import './registration view.scss';
+
+export function RegistrationView(props) {
+    const [name, setName] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [birthday, setBirthday] = useState('');
+};
+
+return (
+    <div className="registration-view">
+        <Form>
+            {formField('Name', name, setName)}
+            {formField('Username', username, setUsername)}
+            {formField('Password', password, setPassword)}
+            {formField('Email', email, setEmail)}
+            {formField('Birthday', birthday, setBirthday)}
+
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
+    </div>
+);
