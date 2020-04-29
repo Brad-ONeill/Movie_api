4a5d5a581b1ac843ecacfc3c567bfede98b54d9e
Brad <49478841+Brad-ONeill@users.noreply.github.com>
2020-04-29 17:10:59 +0100

    removed styling from card and added it to scss file

--- client/src/components/movie-card/movie-card.jsx
+++ client/src/components/movie-card/movie-card.jsx
@@ -3,19 +3,23 @@ import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+import './movie-card.scss'; //import styling
+
 export class MovieCard extends React.Component {
     render() {
         const { movie, onClick } = this.props;
 
         return (
-            <Card style={{ width: '16rem' }}>
-                <Card.Img variant="top" src={movie.ImagePath} />
-                <Card.Body>
-                    <Card.Title>{movie.Title}</Card.Title>
-                    <Card.Text>{movie.Description}</Card.Text>
-                    <Button onClick={() => onClick(movie)} variant="link">Open</Button>
-                </Card.Body>
-            </Card>
+            <cardContainer className="cardContainer">
+                <Card className="movieCard">
+                    <Card.Img className="imageCard" variant="top" src={movie.ImagePath} />
+                    <Card.Body>
+                        <Card.Title>{movie.Title}</Card.Title>
+                        <Card.Text>>{movie.Description}</Card.Text>
+                        <Button className="button" onClick={() => onClick(movie)} variant="link">Open</Button>
+                    </Card.Body>
+                </Card>
+            </cardContainer>
         );
     }
 }
