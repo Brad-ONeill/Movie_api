4e0dd2fcdea72bdddc03acdf67f713da4fbdb6c7
Brad <49478841+Brad-ONeill@users.noreply.github.com>
2020-05-15 17:25:38 +0100

    Added axios and post methods

--- client/src/components/registration-view/registration-view.jsx
+++ client/src/components/registration-view/registration-view.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
+//Import stylings
 import './registration-view.scss';
 
 export function RegistrationView(props) {
@@ -11,8 +14,20 @@ export function RegistrationView(props) {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(username, password, email, birthday);
-        props.onRegistered({ username, password, email, birthday });
+        axios.post('https://dashboard.heroku.com/apps/limitless-thicket-23479/users', {
+            Username: username,
+            Password: password,
+            Email: email,
+            Birthday: birthday
+        })
+            .then(response => {
+                const data = response.data;
+                console.log(data);
+                window.open('/', '_self'); // '_self' is necessary to open the page in the current tab
+            })
+            .catch(e => {
+                console.log('error registering the user')
+            });
     };
 
     return (
@@ -21,24 +36,24 @@ export function RegistrationView(props) {
                 <Form.Row>
                     <Form.Group as={Col} className="col-12 col-sm-12 col-md-6" controlId="username">
                         <Form.Label>Username</Form.Label>
-                        <Form.Control type="text" placeholder="Username" />
+                        <Form.Control type="text" placeholder="Username" value={username} onChange={e => createUsername(e.target.value)} />
                     </Form.Group>
 
                     <Form.Group as={Col} className="col-12 col-sm-12 col-md-6" controlId="password" >
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Enter your password" />
+                        <Form.Control type="password" placeholder="Enter your password" value={password} onChange={e => createPassword(e.target.value)} />
                     </Form.Group >
                 </Form.Row>
 
                 <Form.Row>
                     <Form.Group as={Col} className="col-12 col-sm-12 col-md-6" controlId="birthday">
                         <Form.Label>Date of Birth</Form.Label>
-                        <Form.Control type="date" placeholder="13/05/2020" />
+                        <Form.Control type="date" placeholder="13/05/2020" value={birthday} onChange={e => createBirthday(e.target.value)} />
                     </Form.Group>
 
                     <Form.Group as={Col} className="col-12 col-sm-12 col-md-6" controlId="email" >
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter Email" />
+                        <Form.Control type="email" placeholder="Enter Email" value={email} onChange={e => createEmail(e.target.value)} />
                         <Form.Text className="text-muted"> We won't share your info or contact you</Form.Text>
                     </Form.Group>
                 </Form.Row>
@@ -47,7 +62,7 @@ export function RegistrationView(props) {
                     <Form.Check type="checkbox" label="Check this box to confirm your details are correct" />
                 </Form.Group>
 
-                <Button varient="primary" type="submit" > Submit</Button >
+                <Button varient="primary" type="submit" onClick={handleSubmit}> Submit</Button >
 
             </Form >
         </Container>
