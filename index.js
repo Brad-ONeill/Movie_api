b15b00b7130987d84cfe0b04597a93b21979f54a
Brad <49478841+Brad-ONeill@users.noreply.github.com>
2020-05-28 10:19:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const express = require("express"),
 const app = express();
 const cors = require("cors");
 app.use(cors());
-let allowedOrigins = ["http://localhost:8080", "http://localhost:1234"];
+let allowedOrigins = ["http://localhost:1234"];
 
 app.use(
   cors({
