73be84883530d7714da34776693ed2f7366bcd22
Brad <49478841+Brad-ONeill@users.noreply.github.com>
2020-05-28 10:32:50 +0100

    Screw it, removed cors entirely

--- index.js
+++ index.js
@@ -10,25 +10,6 @@ const express = require("express"),
   bodyParser = require("body-parser"),
   uuid = require("uuid/v5");
 const app = express();
-const cors = require("cors");
-app.use(cors());
-let allowedOrigins = ["http://localhost:1234"];
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn’t found on the list of allowed origins
-        let message =
-          "The CORS policy for this application doesn’t allow access from origin " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
 
 const { check, validationResult } = require("express-validator");
 
